2969ce38283feb183027896ad1ad05fe43b856f2
Gahja88 <vggonzagafilho@gmail.com>
2025-11-03 12:17:53 -0300

    feat(Projeto): Adicionado a classe de Projeto

---
+++ backend/Classes/Projeto.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+namespace backend.Classes;
+{
+  public class Projeto
+  {
+      public int ProjetoId { get; set; }
+      public string Titulo { get; set; } = string.Empty;
+      public string? Descricao { get; set; }
+      public StatusProjeto Status { get; set; } = StatusProjeto.disponivel;
+      public string? Area { get; set; }
+      public int DonoId { get; set; }
+      public DateTime CriadoEm { get; set; } = DateTime.Now;
+      public DateTime AtualizadoEm { get; set; } = DateTime.Now;
+      public string? Logo { get; set; }
+      
+      // Navigation properties
+      public Usuario? Dono { get; set; }
+      public List<ProjetoMembro>? Membros { get; set; }
+      public List<Documento>? Documentos { get; set; }
+  }
+}
